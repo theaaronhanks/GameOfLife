2a097ce96749ffcd25f8a4191abeb5c813ee010d
Aaron Hanks <theaaronhanks@duck.com>
2023-08-05 22:59:43 -0600

    Add life simulator class with starter tests

---
+++ app/src/main/java/newlife/LifeSim.java
@@ -0,0 +1,114 @@
+package newlife;
+
+import java.lang.Math;
+import newlife.patterns.Pattern;
+
+public class LifeSim {
+    private boolean[][] world;
+
+    public LifeSim(int sizeX, int sizeY) {
+        this.world = new boolean[sizeX][sizeY];
+        for (int i = 0; i < world.length; i++) {
+            for (int j = 0; j < this.world[i].length; j++) {
+                this.world[i][j] = false;
+            }
+        }
+    }
+
+    public void setRandomState() {
+        setRandomState(0.33);
+    }
+
+    public void setRandomState(double liveChance) {
+        for (int i = 0; i < world.length; i++) {
+            for (int j = 0; j < this.world[i].length; j++) {
+                if (Math.random() < liveChance) {
+                    this.world[i][j] = true;
+                }
+            }
+        }
+    }
+
+    public int getSizeX() {
+        return this.world.length;
+    }
+
+    public int getSizeY() {
+        return this.world[0].length;
+    }
+
+    public boolean getCell(int x, int y) {
+        if (x > -1 && x < this.world.length && y > -1 && y < this.world[x].length) {
+            return this.world[x][y];
+        }
+        return false;
+    }
+
+    public void insertPattern(Pattern pattern, int startX, int startY) {
+        for (int i = 0; i < pattern.getSizeX(); i++) {
+            for (int j = 0; j < pattern.getSizeY(); j++) {
+                if (pattern.getCell(i, j)) {
+                    this.world[i + startX][j + startY] = true;
+                }
+            }
+        }
+    }
+
+    private int checkNeighbors(int x, int y) {
+        int count = 0;
+        if (this.getCell(x - 1, y - 1)) {
+            count++;
+        }
+        if (this.getCell(x, y - 1)) {
+            count++;
+        }
+        if (this.getCell(x + 1, y - 1)) {
+            count++;
+        }
+        if (this.getCell(x - 1, y)) {
+            count++;
+        }
+        if (this.getCell(x + 1, y)) {
+            count++;
+        }
+        if (this.getCell(x - 1, y + 1)) {
+            count++;
+        }
+        if (this.getCell(x, y + 1)) {
+            count++;
+        }
+        if (this.getCell(x + 1, y + 1)) {
+            count++;
+        }
+
+        // for (int i = x-1; i< x+2; i++) {
+        // for (int j= y-1; j<y+2; j++) {
+        // if (i==x && j==y) continue;
+        // if (this.getCell(i, j)) count++;
+        // }
+        // }
+        return count;
+    }
+
+    public void update() {
+        boolean[][] nextWorld = new boolean[this.getSizeY()][this.getSizeX()]; //create a new grid
+        for (int i = 0; i < this.world.length; i++) {
+            for (int j = 0; j < this.world[i].length; j++) {
+                int count = checkNeighbors(i, j);
+
+                if (this.getCell(i, j) && ((count == 2) || (count == 3) )) {
+                    nextWorld[i][j] = true;
+                } else if ( !this.getCell(i, j) && count == 3){
+                    nextWorld[i][j] = true;
+                } else {
+                    nextWorld[i][j] = false;
+                }
+            }
+        }
+        for (int i = 0; i < nextWorld.length; i++) {
+            for (int j = 0; j < nextWorld[i].length; j++) {
+                this.world[i][j] = nextWorld[i][j];
+            }
+        }
+    }
+}
---
+++ app/src/test/java/newlife/LifeSimTest.java
@@ -0,0 +1,120 @@
+package newlife;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import newlife.patterns.Pattern;
+
+public class LifeSimTest {
+
+    @Test
+    void TestConstructor() {
+        final int x = 40;
+        final int y = 50;
+
+        LifeSim sim = new LifeSim(x, y);
+        assertEquals(40, sim.getSizeX(), "X is wrong");
+        assertEquals(50, sim.getSizeY(), "X is wrong");
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                assertFalse(sim.getCell(i, j), "Incorrect default state");
+            }
+        }
+    }
+
+    @Test
+    void TestRandomizer() {
+        final int x = 10;
+        final int y = 10;
+
+        LifeSim sim = new LifeSim(x, y);
+        sim.setRandomState(1);
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                assertTrue(sim.getCell(i, j), "Random does not work");
+            }
+        }
+    }
+
+    @Test
+    void TestInsertPatterns() {
+        final int x = 10;
+        final int y = 10;
+        final int startx = 0;
+        final int starty = 2;
+        final int startx2 = 5;
+        final int starty2 = 4;
+
+        final TestPattern1 pattern = new TestPattern1();
+        final TestPattern2 pattern2 = new TestPattern2();
+
+        LifeSim sim = new LifeSim(x, y);
+        sim.insertPattern(pattern, startx, starty);
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                assertEquals(sim.getCell(i, j), data1[i][j], "Wrong pattern in cell");
+            }
+        }
+
+        LifeSim sim2 = new LifeSim(x, y);
+        sim2.insertPattern(pattern2, startx2, starty2);
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                assertEquals(sim2.getCell(i, j), data2[i][j], "Wrong pattern in cell");
+            }
+        }
+
+    }
+
+    boolean data1[][] = {
+            { false, false, true, true, true, true, false, false, false, false },
+            { false, false, true, true, true, true, false, false, false, false },
+            { false, false, true, true, true, true, false, false, false, false },
+            { false, false, true, true, true, true, false, false, false, false },
+            { false, false, false, false, false, false, false, false, false, false },
+            { false, false, false, false, false, false, false, false, false, false },
+            { false, false, false, false, false, false, false, false, false, false },
+            { false, false, false, false, false, false, false, false, false, false },
+            { false, false, false, false, false, false, false, false, false, false },
+            { false, false, false, false, false, false, false, false, false, false }
+    };
+
+    boolean data2[][] = {
+            { false, false, false, false, false, false, false, false, false, false },
+            { false, false, false, false, false, false, false, false, false, false },
+            { false, false, false, false, false, false, false, false, false, false },
+            { false, false, false, false, false, false, false, false, false, false },
+            { false, false, false, false, false, false, false, false, false, false },
+            { false, false, false, false, true, false, true, false, false, false },
+            { false, false, false, false, false, true, false, true, false, false },
+            { false, false, false, false, true, false, true, false, false, false },
+            { false, false, false, false, false, true, false, true, false, false },
+            { false, false, false, false, false, false, false, false, false, false }
+    };
+
+    private class TestPattern1 extends Pattern {
+        public TestPattern1() {
+            this.pattern = new boolean[][] {
+                    { true, true, true, true, },
+                    { true, true, true, true, },
+                    { true, true, true, true, },
+                    { true, true, true, true, }
+            };
+        }
+    }
+
+    private class TestPattern2 extends Pattern {
+        public TestPattern2() {
+            this.pattern = new boolean[][] {
+                    { true, false, true, false, },
+                    { false, true, false, true, },
+                    { true, false, true, false, },
+                    { false, true, false, true, }
+            };
+        }
+    }
+
+}
